4ec67d3f0f6e7b0fd5e9e09fcf02b3d16d3c2ed5
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-08-30 10:08:42 +0530

    Update template-chapters.php

--- wp-content/themes/astra/template-chapters.php
+++ wp-content/themes/astra/template-chapters.php
@@ -29,3 +29,26 @@ if ( astra_page_layout() == 'right-sidebar' ) :
 endif;
 
 get_footer();
+
+<?php
+// Define an array of chapters with titles and content
+$chapters = [
+    [
+        'title' => 'Introduction',
+        'content' => '<p>Welcome to the introduction chapter. Here we will cover the basics.</p>'
+    ],
+    [
+        'title' => 'Chapter 1: Getting Started',
+        'content' => '<p>This chapter will guide you through the initial setup and basic concepts.</p>'
+    ],
+    [
+        'title' => 'Chapter 2: Advanced Topics',
+        'content' => '<p>In this chapter, we delve into more advanced topics and techniques.</p>'
+    ],
+    [
+        'title' => 'Conclusion',
+        'content' => '<p>We summarize the key points covered and discuss the next steps.</p>'
+    ]
+];
+
+?>
