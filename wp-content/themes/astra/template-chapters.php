7e3bc6e3671caae4c434eec0b4920af270da82a9
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-09-06 22:32:59 +0530

    Update template-chapters.php

--- wp-content/themes/astra/template-chapters.php
+++ wp-content/themes/astra/template-chapters.php
@@ -1,20 +1,7 @@
 <?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Astra
- * @since 1.0.0
- */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+	exit;
 }
 
 get_header(); ?>
@@ -33,7 +20,7 @@ get_header(); ?>
 
 		astra_primary_content_bottom();
 		?>
-	</div><!-- #primary -->
+	</div>
 <?php
 if ( astra_page_layout() == 'right-sidebar' ) :
 
